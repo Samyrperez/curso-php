731180d2efd7e8a388c72eccae47dca37add6d54
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2025-01-04 07:12:14 -0500

    data-types

---
+++ data-types.php
@@ -0,0 +1,25 @@
+<?php
+// Data types
+
+// Boleanos
+
+echo TRUE; //aL IMPRIMIR SE REPRESENTA CON EL NUMERO 1
+
+echo FALSE; // Se representa con el numero 0
+
+// Enteros
+
+echo 10;
+
+var_dump(10);//Esta función nos permite saber que clase de dato es
+var_dump(FALSE);
+
+// Float
+
+var_dump(7.6);
+
+// String
+
+var_dump("Texto");
+
+?>
